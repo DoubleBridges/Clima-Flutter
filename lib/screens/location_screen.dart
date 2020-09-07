9041675ff01ea054c4328260b20eedf0e294a71b
Dave Kane <davelkane@gmail.com>
2020-09-06 22:56:38 -0500

    Added text field for searching cities, make API call for city entered and wired up back button

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
 import '../services/weather.dart';
+import '../screens/city_screen.dart';
 
 class LocationScreen extends StatefulWidget {
   LocationScreen({this.locationWeather});
@@ -26,10 +27,17 @@ class _LocationScreenState extends State<LocationScreen> {
 
   void updateUI(dynamic weatherData) {
     setState(() {
+      if (weatherData == null) {
+        temp = 0;
+        weatherIcon = "Error";
+        weatherMessage = 'No network available';
+        cityName = '';
+        return;
+      }
       var condition = weatherData['weather'][0]['id'];
       weatherIcon = weather.getWeatherIcon(condition);
-      double init_temp = weatherData['main']['temp'];
-      temp = init_temp.toInt();
+      double initTemp = weatherData['main']['temp'];
+      temp = initTemp.toInt();
       cityName = weatherData['name'];
       weatherMessage = weather.getMessage(temp);
     });
@@ -57,14 +65,31 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var weatherData = await weather.getLocationWeather();
+                      updateUI(weatherData);
+                    },
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var typedName = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                      if (typedName != null) {
+                        var weatherData =
+                            await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
+                      }
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,31 @@
+import '../services/location.dart';
+import '../services/networking.dart';
+
+const openWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = '41113964a80b843fafb36d9221cc49f2';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherURL?q=$cityName&appid=$apiKey&units=imperial';
+
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherURL?lat=${location.lat}&lon=${location.lon}&appid=$apiKey&units=imperial');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
